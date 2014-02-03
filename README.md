ff13af440fdbbc6330d259332fd2836fa08e6e97
balazs-zsoldos <balazs.zsoldos@everit.biz>
2014-02-03 14:24:59 +0100

    Releasing 2.0.0

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 java-task-killer
 ================
 
-A very simple utility to kill java programs using "jps"
+The aim of this tiny tool is to be able to kill java based programs.
 
-Project site: http://everit.org/java-task-killer
+## Usage
+
+Start your java task in the way that the command contains a special part.
+A good trick is to use a system property that is not used by the programs
+but identifies the process.
+
+E.g.: java -DmyDummyProp=XXX-XXX-XXX -jar myApp.jar
+
+You can kill this task by calling:
+
+java -jar java-task-killer.jar --startCommandPart=XXX-XXX-XXX
+
+The tool uses the jps tool of java. In case the bin directory of the Java
+installation is not on the path, the tool will not work.
+
+## Supported operating systems
+
+On windows the following command is used:
+
+taskkill /F /T /PID
+
+On every other operating systems the following command is used:
+
+kill -2
--- src/main/java/org/everit/tools/javataskkiller/Main.java
+++ src/main/java/org/everit/tools/javataskkiller/Main.java
@@ -1,28 +1,22 @@
+/**
+ * This file is part of Everit - Java Task Killer.
+ *
+ * Everit - Java Task Killer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Everit - Java Task Killer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Everit - Java Task Killer.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.everit.tools.javataskkiller;
 
-/*
- * Copyright (c) 2011, Everit Kft.
- *
- * All rights reserved.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301  USA
- */
-
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -32,7 +26,7 @@ import java.util.List;
  */
 public class Main {
 
-    public static final String OS_WINDOWS = "windows";
+    public static final String ARG_START_COMMAND_PART = "startCommandPart";
 
     public static final String OS_LINUX_UNIX = "linux";
 
@@ -40,12 +34,56 @@ public class Main {
 
     public static final String OS_SUNOS = "sunos";
 
-    public static void main(String[] args) {
-        
-        String uniqueId = System.getProperty("processIdToKill");
-        if (uniqueId == null) {
-            System.out.println("[ERROR] 'processIdToKill' system property is not defined.");
+    public static final String OS_WINDOWS = "windows";
+
+    public static String getOS() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.indexOf("win") >= 0) {
+            return OS_WINDOWS;
         }
+        if (os.indexOf("mac") >= 0) {
+            return OS_MACINTOSH;
+        }
+        if ((os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0)) {
+            return OS_LINUX_UNIX;
+        }
+        if (os.indexOf("sunos") >= 0) {
+            return OS_SUNOS;
+        }
+        return null;
+    }
+
+    private static void killProcess(final String jpsLine) {
+        String[] jpsTokens = jpsLine.split(" ");
+        String processId = jpsTokens[0];
+        System.out.println("Killing process with id " + processId);
+        String os = getOS();
+        if (OS_WINDOWS.equals(os)) {
+            try {
+                Runtime.getRuntime().exec("taskkill /F /T /PID " + processId);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            try {
+                Runtime.getRuntime().exec("kill -2 " + processId);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void main(final String[] args) {
+        if (args.length != 1) {
+            System.err.println("[ERROR] There should be one argument, the --" + ARG_START_COMMAND_PART + "=xxx");
+        }
+
+        String startCommandPartArg = args[0];
+        if (!startCommandPartArg.startsWith("--" + ARG_START_COMMAND_PART + "=")) {
+            System.err.println("[ERROR] There should be one argument: --" + ARG_START_COMMAND_PART + "=xxx");
+        }
+
+        String startCommandPart = startCommandPartArg.substring(("--" + ARG_START_COMMAND_PART + "=").length());
 
         try {
             Process jpsProcess = Runtime.getRuntime().exec("jps -mlvV");
@@ -64,56 +102,14 @@ public class Main {
             List<String> jpsResultLines = stdOutDumper.retrieveLines(null);
 
             for (String jpsResultLine : jpsResultLines) {
-                if (jpsResultLine.contains(uniqueId) && !jpsResultLine.contains("processIdToKill")) {
+                if (jpsResultLine.contains(startCommandPart) && !jpsResultLine.contains(ARG_START_COMMAND_PART)) {
                     killProcess(jpsResultLine);
                 }
             }
         } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(2);
+            throw new RuntimeException(e);
         } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-    }
-
-    private static void killProcess(String jpsLine) {
-        String[] jpsTokens = jpsLine.split(" ");
-        String processId = jpsTokens[0];
-        System.out.println("Killing process with id " + processId);
-        String os = getOS();
-        if (OS_WINDOWS.equals(os)) {
-            try {
-                Runtime.getRuntime().exec("taskkill /F /T /PID " + processId);
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        } else {
-            try {
-                Runtime.getRuntime().exec("kill -2 " + processId);
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-
-    }
-
-    public static String getOS() {
-        String os = System.getProperty("os.name").toLowerCase();
-        if (os.indexOf("win") >= 0) {
-            return OS_WINDOWS;
-        }
-        if (os.indexOf("mac") >= 0) {
-            return OS_MACINTOSH;
-        }
-        if ((os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0)) {
-            return OS_LINUX_UNIX;
-        }
-        if (os.indexOf("sunos") >= 0) {
-            return OS_SUNOS;
-        }
-        return null;
     }
 }
